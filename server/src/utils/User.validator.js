60394edd7b30149c746b9188635214cad030a7bc
AnastasiaDzam <yorklo@mail.ru>
2024-12-05 15:52:21 +0300

    Fix everything for dev

--- server/src/utils/User.validator.js
+++ server/src/utils/User.validator.js
@@ -1,47 +1,93 @@
-//функция, которая проверяет корректность поступающих данных; 
 class UserValidator {
-    static validate(data) {
-        const { userName, userLastName, email, password, registration } = data;
-
-        if (!userName || typeof userName !== 'string' || userName.trim() === '') {
-            return {
-                isValid: false,
-                error: 'userName is required and must be a non-empty string',
-            }
-         } 
-         else if
-        (!userLastName || typeof userLastName !== 'string' || userLastName.trim() === '') {
-            return {
-                    isValid: false,
-                    error: 'userLastName is required and must be a non-empty string',
-            }
-        }
-        else if
-        (!email || typeof email !== 'string' || email.trim() === '') {
-            return {
-                    isValid: false,
-                    error: 'email is required and must be a non-empty string',
-            }
-        }
-        else if
-        (!password || typeof password !== 'string' || password.trim() === '') {
-            return {
-                    isValid: false,
-                    error: 'password is required and must be a non-empty string',
-            }
-        }
-        else if
-        (!registration || typeof registration !== 'string' || registration.trim() === '') {
-            return {
-                    isValid: false,
-                    error: 'registration is required and must be a non-empty string',
-            }
-        }
+    static validateSignUp(data) {
+      const { username, email, password } = data;
+      if (!username || typeof username !== 'string' || username.trim() === '') {
         return {
-            isValid: true,
-            error: null
-        }
+          isValid: false,
+          error: 'Username is required and must be a non-empty string.',
+        };
+      }
+      if (
+        !email ||
+        typeof email !== 'string' ||
+        email.trim() === '' ||
+        !this.validateEmail(email)
+      ) {
+        return {
+          isValid: false,
+          error:
+            'Email is required, must be a non-empty string, and must be a valid email address.', // Более детальное сообщение об ошибке
+        };
+      }
+      if (
+        !password ||
+        typeof password !== 'string' ||
+        password.trim() === '' ||
+        !this.validatePassword(password)
+      ) {
+        return {
+          isValid: false,
+          error:
+            'Password is required, must be a non-empty string, contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character.', // Подробное сообщение об ошибке
+        };
+      }
+      return {
+        isValid: true,
+        error: null,
+      };
     }
-}
-
-module.exports = UserValidator;
+    static validateSignIn(data) {
+      const { email, password } = data;
+  
+      if (
+        !email ||
+        typeof email !== 'string' ||
+        email.trim() === '' ||
+        !this.validateEmail(email)
+      ) {
+        return {
+          isValid: false,
+          error: 'Email is required and must be a valid email address.',
+        };
+      }
+      if (!password || typeof password !== 'string' || password.trim() === '') {
+        return {
+          isValid: false,
+          error: 'Password is required and must not be an empty string.',
+        };
+      }
+      return {
+        isValid: true,
+        error: null,
+      };
+    }
+    static validateEmail(email) {
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailPattern.test(email);
+    }
+  
+    static validatePassword(password) {
+      // Регулярные выражения для проверки требований к паролю
+      const hasUpperCase = /[A-Z]/; // Проверка на наличие заглавной буквы
+      const hasLowerCase = /[a-z]/; // Проверка на наличие строчной буквы
+      const hasNumbers = /\d/; // Проверка на наличие цифры
+      const hasSpecialCharacters = /[!@#$%^&*()-,.?":{}|<>]/; // Проверка на наличие спецсимвола
+      const isValidLength = password.length >= 8; // Проверка на минимальную длину
+  
+      // Проверка всех условий
+      if (
+        !hasUpperCase.test(password) ||
+        !hasLowerCase.test(password) ||
+        !hasNumbers.test(password) ||
+        !hasSpecialCharacters.test(password) ||
+        !isValidLength
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+  
+  module.exports = UserValidator;
+  
