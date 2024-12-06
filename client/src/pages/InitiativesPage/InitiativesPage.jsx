62fbc908268f03b271be7f791a1e74c4585c9d26
AnastasiaDzam <yorklo@mail.ru>
2024-12-06 17:36:58 +0300

    404

--- client/src/pages/InitiativesPage/InitiativesPage.jsx
+++ client/src/pages/InitiativesPage/InitiativesPage.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
-import InitiativesList from '../../widgets/InitiativesList/InitiativesList';
-import InitiativeForm from '../../widgets/InitiativeForm/InitiativeForm';
-import { message as antMessage } from 'antd';
-import InitiativeApi from '../../entities/initiative/InitiativeApi';
-import Dropdown from '../../widgets/Dropdown/Dropdown';
+import { useEffect, useState } from "react";
+import InitiativesList from "../../widgets/InitiativesList/InitiativesList";
+import InitiativeForm from "../../widgets/InitiativeForm/InitiativeForm";
+import { message as antMessage } from "antd";
+import InitiativeApi from "../../entities/initiative/InitiativeApi";
+import Dropdown from "../../widgets/Dropdown/Dropdown";
 
 export default function InitiativesPage({ user }) {
   const [initiatives, setInitiatives] = useState([]);
   const [viewInit, setViewInit] = useState(initiatives);
   const [loading, setLoading] = useState(false);
-  
+
   useEffect(() => {
     loadInitiatives();
   }, []);
@@ -18,7 +18,8 @@ export default function InitiativesPage({ user }) {
   const loadInitiatives = async () => {
     setLoading(true);
     try {
-      const { data, message, error, statusCode } = await InitiativeApi.getInitiatives();
+      const { data, message, error, statusCode } =
+        await InitiativeApi.getInitiatives();
       if (error) {
         antMessage.error(error);
         return;
@@ -28,26 +29,36 @@ export default function InitiativesPage({ user }) {
         setInitiatives(data);
         setViewInit(data);
       }
-
     } catch (error) {
       antMessage.error(error.message);
       console.log(error);
     } finally {
-      antMessage.info('Загрузка завершена');
+      antMessage.info("Загрузка завершена");
       setLoading(false);
     }
   };
 
-
-  console.log("page22",initiatives);
-  
-
   return (
     <div>
-    <Dropdown setViewInit={setViewInit} viewInit={viewInit} initiatives={initiatives}/>
+      <Dropdown
+        setViewInit={setViewInit}
+        viewInit={viewInit}
+        initiatives={initiatives}
+      />
       {loading && <h3>Загрузка...</h3>}
-      {<InitiativeForm setInitiatives={setInitiatives}  setViewInit={setViewInit} setLoading={setLoading} />}
-      <InitiativesList initiatives={viewInit} setInitiatives={setInitiatives} user={user} setViewInit={setViewInit} />
+      {
+        <InitiativeForm
+          setInitiatives={setInitiatives}
+          setViewInit={setViewInit}
+          setLoading={setLoading}
+        />
+      }
+      <InitiativesList
+        initiatives={viewInit}
+        setInitiatives={setInitiatives}
+        user={user}
+        setViewInit={setViewInit}
+      />
     </div>
   );
 }
